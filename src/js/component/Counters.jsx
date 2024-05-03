21ae09e660c31a2951a23fa3c60c1a6c85996e8a
Jhonny Lozano <110267545+lozanosj@users.noreply.github.com>
2024-05-03 14:58:17 +0000

    ready

--- src/js/component/Counters.jsx
+++ src/js/component/Counters.jsx
@@ -1,25 +1,72 @@
+// Contador.jsx
 import React, { useState, useEffect } from 'react';
+import styles from './Counter.module.css';
 
-const Counterx = () => {
-  const [seconds, setSeconds] = useState(0);
+function Counter() {
+const [cuentaAtras, setCuentaAtras] = useState([0,0,0,0]);
+const [pausa,setPausa] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds(seconds => seconds + 1);
+useEffect(() =>{
+    let intervaloID;
+    if(!pausa){ intervaloID = setInterval(() => {
+
+        setCuentaAtras(prevCuenta => {
+            const newCuentaAtras = [...prevCuenta];
+            newCuentaAtras[3] += 1;
+
+            if(newCuentaAtras[3] === 10){
+                newCuentaAtras[3] = 0;
+                newCuentaAtras[2] += 1;
+
+                if(newCuentaAtras[2] === 10){
+                    newCuentaAtras[2] = 0;
+                    newCuentaAtras[1] += 1;
+
+                    if(newCuentaAtras[1] === 10){
+                        newCuentaAtras[1] = 0;
+                        newCuentaAtras[0] += 1;
+
+                        if(newCuentaAtras[0] === 10){
+                            return[0,0,0,0];
+                        }
+                    }
+                }
+            }
+            return newCuentaAtras;});
     }, 1000);
+}
+return () => clearInterval(intervaloID);
+},[pausa])
 
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div>
-      <h1>Contador de segundos</h1>
-      <p>{seconds}</p>
-    </div>
-  );
+const darlePausa = () => {
+    setPausa(!pausa);
 };
 
-export default Counterx;
+const reiniciarContador = () => {
+    setCuentaAtras([0, 0, 0, 0]);
+    setCorrer(true);
+};
+
+    return (
+        <>
+        <div className={`${styles.contenedor}`}>
+            <div className={`${styles.reloj}`}>
+            <i class="far fa-clock"></i>
+            </div>
+            <div className='cuatro'>{cuentaAtras}</div>
+        </div>
+        <div className={`${styles.divbotones}`}>
+            <div>
+            <button className={`${styles.button}`} onClick={darlePausa}><i class="fas fa-pause"></i></button>
+            </div>
+            <div>
+        <button className={`${styles.button}`} onClick={reiniciarContador}><i class="fas fa-redo-alt"></i></button>
+            </div>
+        </div>
 
 
+        </>
+    )
+}
 
+export default Counter
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import Counterx from "./Counters";
-import Counter from "./Ejemplo";
+import Counter from "./Counters.jsx"
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<Counter />
-		</div>
+		<Counter/>
 	);
 };
 
